7bd2312e74daebafcc8bd882e063b66e996bdc46
facedrop <38283957+facedrop@users.noreply.github.com>
2022-05-12 16:38:11 +0300

    js

---
+++ question/app.js
@@ -0,0 +1,37 @@
+// Using selector inside the element
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function(question) {
+    const btn = question.querySelector(".question-btn");
+    btn.addEventListener("click", function() {
+        questions.forEach(function(item) {
+            if(item !== questions) {
+                item.classList.remove("show-text");
+            }
+        })
+
+
+
+        question.classList.toggle("show-text");
+    });
+});
+
+
+
+
+
+
+
+
+
+
+// Traversing the DOM 
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function(btn) {
+//     btn.addEventListener("click",function(e) {
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle("show-text");
+//     });
+// });
